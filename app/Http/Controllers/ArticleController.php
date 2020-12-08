c6160a6406fe1a27aa967fb1a2cce67d4597340b
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2020-12-08 21:19:03 +0900

    記事の投稿機能を実装した

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+// Requestの使用宣言
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     //
 
+    // 記事一覧画面の表示
     public function index()
     {
       $articles = Article::all()->sortByDesc('created_at');
@@ -17,5 +20,32 @@ class ArticleController extends Controller
       // キーに対してのvalueを$articlesで指定している
       // これによりbladeで'articles'が使用できる。
       return view('articles.index', ['articles' => $articles]);
+    }
+
+    // 記事投稿作成画面の表示
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    // 記事の保存に関する処理
+    // 引数にクラスと変数を記述する事で、インスタンスが自動生成される
+    // $article = new Article(); と同じ意味に
+    public function store(ArticleRequest $request, Article $article)
+    {
+        // $article->~はDBへの記述で、$request->~は送られてきたデータ
+        // $article->title = $request->title;
+        // $article->body = $request->body;
+
+        // allメソッドを使用する事で全件データを取得する
+        // fillメソッドに上記の配列を渡すと、指定しておいたプロパティのみが代入される
+        // これでクライアントからの不正リクエストをブロックできる点と、冗長なコードを回避する
+        $article->fill($request->all());
+        // ログイン済みのユーザーが送信したリクエストであれば、userメソッドを使うことでUserクラスのインスタンスにアクセスできる
+        $article->user_id = $request->user()->id;
+        // 最後にモデルのsaveメソッドを使用し、articleテーブルにデータを保存する
+        $article->save();
+        // 投稿保存後、redirectでarticle.indexへ遷移させる
+        return redirect()->route('articles.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,10 @@
 |
 */
 
-Auth::routes(); 
-Route::get('/', 'ArticleController@index');
+Auth::routes();
+Route::get('/', 'ArticleController@index')->name('articles.index');
+// except = 指定のアクションを覗く事ができる
+// 今回articleのindexが重複するため、'/'を優先した
+// middlewareはクライアントからのリクエストに対して、コントローラーで処理する前に処理を行う作業。
+// 'auth'をつける事で、ユーザーがログイン済みかをチェックする。
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
